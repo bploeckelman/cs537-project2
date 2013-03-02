12c32e19aa4e47b8b3fccbda7ea6c63891a12cd7
Brian Ploeckelman <ploeckelman@wisc.edu>
2013-03-02 14:30:05 -0600

    Track statistics (page faults, disk reads/writes), and handle the simple case of npages == nframes

--- main.c
+++ main.c
@@ -15,10 +15,27 @@ how to use the page table and disk interfaces.
 #include <string.h>
 #include <errno.h>
 
+unsigned int equal_pages_and_frames = 0;
+
+struct stats {
+    int page_faults;
+    int disk_reads;
+    int disk_writes;
+};
+struct stats stats;
+
+void print_stats();
+
+
 void page_fault_handler( struct page_table *pt, int page )
 {
-	printf("page fault on page #%d\n",page);
-	exit(1);
+    if (equal_pages_and_frames) {
+        page_table_set_entry(pt, page, page, PROT_READ | PROT_WRITE);
+        ++stats.page_faults;
+    } else {
+        printf("unhandled page fault on page #%d\n",page);
+        exit(1);
+    }
 }
 
 int main( int argc, char *argv[] )
@@ -31,6 +48,9 @@ int main( int argc, char *argv[] )
 	int npages = atoi(argv[1]);
 	int nframes = atoi(argv[2]);
 	const char *program = argv[4];
+
+    if (npages == nframes) equal_pages_and_frames = 1;
+    memset(&stats, 0, sizeof(struct stats));
 
 	struct disk *disk = disk_open("myvirtualdisk",npages);
 	if(!disk) {
@@ -47,7 +67,7 @@ int main( int argc, char *argv[] )
 
 	char *virtmem = page_table_get_virtmem(pt);
 
-	char *physmem = page_table_get_physmem(pt);
+	//char *physmem = page_table_get_physmem(pt);
 
 	if(!strcmp(program,"sort")) {
 		sort_program(virtmem,npages*PAGE_SIZE);
@@ -66,5 +86,17 @@ int main( int argc, char *argv[] )
 	page_table_delete(pt);
 	disk_close(disk);
 
+    print_stats(&stats);
+
 	return 0;
 }
+
+
+void print_stats() {
+    printf("\nStatistics:\n");
+    printf("=====================\n");
+    printf("Page faults = %d\n", stats.page_faults);
+    printf("Disk reads  = %d\n", stats.disk_reads);
+    printf("Disk writes = %d\n", stats.disk_writes);
+}
+
